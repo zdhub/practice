242ebe65a7d03f1bc20843bd29f708c54835065b
YiZhendong <Yi_zhendong@163.com>
2019-06-19 00:01:34 +0800

    test

--- src/main/java/leetcode/Solution32.java
+++ src/main/java/leetcode/Solution32.java
@@ -14,16 +14,17 @@ public class Solution32 {
     public static void main(String[] args) {
 
     }
+
     public static int longestValidParentheses(String s) {
         int maxLen = 0;
         Stack<Integer> stack = new Stack<>();
         stack.push(-1);
-        for (int i = 0;i<s.length();i++){
-            if (s.charAt(i) == '('){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
                 stack.push(i);
             } else {
                 stack.pop();
-                if (stack.isEmpty()){
+                if (stack.isEmpty()) {
                     stack.push(i);
                 } else {
                     maxLen = Math.max(maxLen, i - stack.peek());
