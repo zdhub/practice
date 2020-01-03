c8b7b4ab71f5e6487d3b47c4c8ad63b2f2ec40af
yizhendong <myyizhendong@gmail.com>
2020-01-03 15:52:21 +0800

    完善LRU算法

--- src/main/java/com/example/spring/practice/LRUCacheByListAndMap.java
+++ src/main/java/com/example/spring/practice/LRUCacheByListAndMap.java
@@ -22,6 +22,11 @@ public class LRUCacheByListAndMap {
         private Node next;
         private int key;
         private int value;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 
     private int capacity;
@@ -32,7 +37,62 @@ public class LRUCacheByListAndMap {
     public LRUCacheByListAndMap(int capacity) {
         this.capacity = capacity;
         nodeMap = new HashMap<>();
+    }
 
+    public void set(int key, int value) {
+        Node nodeTemp = new Node(key, value);
+        if (head == null) {
+            head = nodeTemp;
+            nodeMap.put(key, nodeTemp);
+            return;
+        }
+        if (nodeMap.containsKey(key)) {
+            Node node = nodeMap.get(key);
+            node.value = value;
+            insertToHead(node);
+            return;
+        }
+        addNode(nodeTemp);
+
+        if (nodeMap.size() > capacity) {
+            removeLastNode();
+        }
+    }
+
+    public Integer get(int key) {
+        if (nodeMap.get(key) == null) {
+            return null;
+        }
+        insertToHead(nodeMap.get(key));
+        return nodeMap.get(key).getValue();
+
+    }
+
+    private void removeLastNode() {
+        nodeMap.remove(tail.getKey());
+        tail = null;
+    }
+
+    private void addNode(Node nodeTemp) {
+        nodeTemp.next = head;
+        head.prev = nodeTemp;
+        nodeMap.put(nodeTemp.getKey(), nodeTemp);
+    }
+
+    private void insertToHead(Node node) {
+        if (node.prev == null) {
+            return;
+        }
+        if (node.next == null) {
+            head = node.next;
+            node.prev = null;
+        } else {
+            head.prev.next = head.next;
+            head.next.prev = head.prev;
+        }
+
+        head.prev = node;
+        node.next = head;
     }
 
 }
