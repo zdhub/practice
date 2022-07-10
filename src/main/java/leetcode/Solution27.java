6a9799a5df4dc2ca434a5c20f77945ce62079ed9
yizhendong <myyizhendong@gmail.com>
2022-07-10 15:49:46 +0800

    feat: add leetcode 27(question 46)

--- src/main/java/leetcode/Solution27.java
+++ src/main/java/leetcode/Solution27.java
@@ -58,4 +58,22 @@ public class Solution27 {
         return i;
     }
 
+    public int removeElementV3(int[] nums, int val) {
+        int i = 0;
+        int j = nums.length - 1;
+        while(i <= j){
+            while(i < nums.length && nums[i] != val){
+                i++;
+            }
+            while(j >= 0 && nums[j] == val){
+                j--;
+            }
+            if(i < j){
+                nums[i] = nums[j];
+                nums[j] = val;
+            }
+        }
+        return i;
+    }
+
 }
