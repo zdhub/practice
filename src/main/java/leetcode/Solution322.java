e0850c1faec8e0ac31aa797bf10e790de61134eb
yizhendong <yizhendong@bytedance.com>
2023-06-09 13:56:03 +0800

    feat: add comment for dynamic pragramming
    Change-Id: Ice2616694835479661202bb6ead42bd917bc09f5

--- src/main/java/leetcode/Solution322.java
+++ src/main/java/leetcode/Solution322.java
@@ -99,6 +99,41 @@ class Solution322 {
 
 
     /**
+     * 还有一种比较容易理解的解法
+     */
+    int[] memo;
+
+    int coinChangeV3(int[] coins, int amount) {
+        memo = new int[amount + 1];
+        // 备忘录初始化为一个不会被取到的特殊值，代表还未被计算
+        Arrays.fill(memo, -666);
+
+        return dp(coins, amount);
+    }
+
+    int dp(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        if (amount < 0) return -1;
+        // 查备忘录，防止重复计算
+        if (memo[amount] != -666)
+            return memo[amount];
+
+        int res = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            // 计算子问题的结果
+            int subProblem = dp(coins, amount - coin);
+            // 子问题无解则跳过
+            if (subProblem == -1) continue;
+            // 在子问题中选择最优解，然后加一
+            res = Math.min(res, subProblem + 1);
+        }
+        // 把计算结果存入备忘录
+        memo[amount] = (res == Integer.MAX_VALUE) ? -1 : res;
+        return memo[amount];
+    }
+
+
+    /**
      *
      * 方式一：不带备忘录的穷举法
      *
